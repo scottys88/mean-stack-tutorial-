eaf87237b9fc649abacf693aefe48c3013798989
scottys88 <scott_schubert@hotmail.com.au>
2018-12-19 21:42:17 +1030

    Up to pt 3 of the tutorial

---
+++ my-new-angular-app/src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  doubleClick: Hero;
+  
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    if(this.selectedHero != hero) {
+      this.selectedHero = hero;
+    }
+    else {
+      this.doubleClick = hero;
+    }
+}
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
